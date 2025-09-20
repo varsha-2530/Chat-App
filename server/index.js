0bd607e86de53657bb0039400c374c26ae168c51
varsha-2530 <vb8607518@gmaail.com>
2025-09-20 18:32:52 +0530

    server Port add

--- server/index.js
+++ server/index.js
@@ -5,3 +5,11 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
